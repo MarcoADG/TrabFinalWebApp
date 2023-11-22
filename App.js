68a2f17c8720c3afe70f53c00037dcfc8cd620f6
Rofogale <marcoaureliodgomes@gmail.com>
2023-11-21 21:08:45 -0300

    fazendo check de conecção

--- App.js
+++ App.js
@@ -1,8 +1,33 @@
 import { NavigationContainer } from "@react-navigation/native";
 import AuthProvider from "./src/contexts/auth";
 import RoutesStack from "./src/routes/StackNavigator";
+import React, { useEffect, useState } from "react";
+import NetInfo from "@react-native-community/netinfo";
+import { Alert } from "react-native";
 
 export default function App() {
+  const [isConnected, setIsConnected] = useState(true);
+
+  // Efeito colateral para escutar as alterações na conexão à internet
+  useEffect(() => {
+    // Adiciona um ouvinte para atualizações de conexão
+    const unsubscribe = NetInfo.addEventListener((state) => {
+      // Atualiza o estado com o status atual da conexão
+      setIsConnected(state.isConnected);
+      Alert.alert(
+        isConnected
+          ? "Você está conectado à internet"
+          : "Sem conexão com a internet!"
+      );
+    });
+
+    // Função de limpeza que será executada quando o componente for desmontado
+    return () => {
+      // Remove o ouvinte para evitar vazamentos de memória.  Usubscribe é uma palavra usada em React para se referir à função de limpeza no useEffect.
+      unsubscribe();
+    };
+  }, []);
+
   return (
     <NavigationContainer>
       <AuthProvider>
